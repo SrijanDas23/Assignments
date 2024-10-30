b574689bc01e07ffecae236ca24334d205dd827c
Srijan Das <157733127+SrijanDas23@users.noreply.github.com>
2024-10-30 15:48:30 +0530

    made the check and next buttons functionable

--- Assignment 8 quiz application/question.js
+++ Assignment 8 quiz application/question.js
@@ -5,8 +5,24 @@ let correctAnswer=null;
 let score=null;
 let data=null;
 let currentQuestion=0;
+let currentScore=0;
+let time=null;
+let timerInterval=null;
+let checkButton=null;
+let nextButton=null;
+let selectedChoice=null;
+const main=document.querySelector('main');
+const timerDisplay=document.getElementById('time-left');
+
+const firstRemark=document.getElementById('first-remark');
+const secondRemark=document.getElementById('second-remark');
 
 (async()=>{
+    await fetchQuestion();
+    setName();
+})();
+
+async function fetchQuestion(){
     try{
         const response=await fetch("data.json");
         data=await response.json();
@@ -24,11 +40,25 @@ let currentQuestion=0;
         setTopic();
         setQuestion();
         setChoices();
+        startTimer();
+        clearSelectedAnswers();
     }
     catch(err){
         console.log(err);
     }
-})();
+}
+
+
+function setName(){
+    const displayedName=document.getElementById('displayedName');
+    const userName=localStorage.getItem('userName');
+
+    if(userName){
+        const span=document.createElement('span');
+        span.innerHTML=`, ${userName}`;
+        displayedName.appendChild(span);
+    }
+}
 
 function setTopic(){
     const topicContent=document.getElementById('topic');
@@ -37,6 +67,8 @@ function setTopic(){
 
 function setQuestion(){
     const questionContent=document.getElementById('question');
+    const questionNo=document.getElementById('question-no');
+    questionNo.innerHTML=currentQuestion+1;
     questionContent.innerHTML=question;
 }
 
@@ -47,21 +79,78 @@ function setChoices() {
     document.getElementById('choice4').innerHTML=choices[3];
 }
 
-function showCheckButton(){
-    const main=document.querySelector('main');
-    const checkButton=document.createElement('button');
+function showCheckButton(selectedAnswer){
+    if(checkButton && main.contains(checkButton)){
+        main.removeChild(checkButton);
+    }
+    clearSelectedAnswers();
+    selectedAnswer.classList.add('selected');
+    checkButton=document.createElement('button');
     checkButton.innerHTML='Check';
+    checkButton.id='checkButton';
     main.appendChild(checkButton);
+
+    selectedChoice=selectedAnswer.innerText;
+    checkButton.addEventListener('click', checkAnswer);
 }
 
-
-document.addEventListener('DOMContentLoaded', () => {
-    const displayedName=document.getElementById('displayedName');
-    const userName=localStorage.getItem('userName');
-
-    if(userName){
-        const span=document.createElement('span');
-        span.innerHTML=`, ${userName}`;
-        displayedName.appendChild(span);
+function checkAnswer(){
+    console.log(selectedChoice);
+    console.log(choices[correctAnswer]);
+    if(selectedChoice===choices[correctAnswer]){
+        console.log("Correct Answer");
+        currentScore+=score;
+        const scoreElement=document.getElementById('score');
+        scoreElement.innerHTML=currentScore;
+        firstRemark.innerHTML='Correct Answer';
+        firstRemark.classList.add('correct');
     }
-});
+    else{
+        firstRemark.classList.add('wrong');
+        firstRemark.innerHTML='Wrong Answer';
+    }
+    secondRemark.innerHTML='Please click on Next';
+
+    main.removeChild(checkButton);
+    nextButton=document.createElement('button');
+    nextButton.innerHTML='Next';
+    nextButton.id='nextButton';
+    main.appendChild(nextButton);
+    nextButton.addEventListener('click', ()=>{
+        main.removeChild(nextButton);
+        console.log("Next Question");
+        currentQuestion++;
+        if(currentQuestion<data.questions.length){
+            console.log("Next Question");
+            fetchQuestion();
+        }
+    });
+}
+
+function startTimer(){
+    time=60;
+    if(timerInterval){
+        clearInterval(timerInterval);
+    }
+
+    timerInterval=setInterval(()=>{
+        if (time>0){
+            time--;
+            timerDisplay.innerHTML=`00:${time}s`;
+        } else {
+            clearInterval(timerInterval);
+            time=60;
+            currentQuestion++;
+            if(currentQuestion<data.questions.length){
+                fetchQuestion();
+            }
+        }
+    }, 1000);
+}
+
+function clearSelectedAnswers(){
+    const answers=document.getElementsByClassName('answer');
+    Array.from(answers).forEach(answer => {
+        answer.classList.remove('selected');
+    });
+}
